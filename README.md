80170a151e7272ef473810dc2049835a5500487b
FlyingSheep22 <kevinbai222@gmail.com>
2025-01-06 19:34:01 -0500

    Update readme and added Last Call project

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# React + Vite
+# Swimmingsheep.com - Portfolio Website
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Learn more about me and the projects I've worked.
+Created using:
+- React.js
+- Vite
+- Github Pages
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Project from "../components/Project"
 
+import lastcall from "../assets/images/projects/LastCall.png";
 import site from "../assets/images/projects/site_home.png";
 import siteAbout from "../assets/images/projects/site_aboutme.png";
 import duck from "../assets/images/projects/proDUCKtive.jpg";
@@ -23,15 +24,24 @@ function Projects(){
                 name={"SwimmingSheep.com"}
                 tools={"React, HTML/CSS, Node.js"}
                 images={[site, siteAbout]}
-                description={"The current wesbite you are visiting! Self taught and developed fully with React.js as a first step into web dev."}
+                description={"The current wesbite you are visiting! Fully developed with React.js."}
                 link={"https://github.com/FlyingSheep22/swimmingsheep"}
                 linkName={"Github Repository"}
+            />
+
+            <Project
+                name={"Last Call"}
+                tools={"Unity, C#, Cinemachine, Unity UI, Aseprite"}
+                images={[lastcall, lastcall]}
+                description={"Roguelike survival game solo developed in Unity, with assets created in Aseprite and Procreate. Extensively explored Unity's UI and cinemachine frameworks to create custom menus and camera effects."}
+                link={"https://flyingsheep22.itch.io/last-call"}
+                linkName={"Itch.io page (Try it out!)"}
             />
 
             <Project 
                 name={"ProDUCKtive"}
                 tools={"Unity, C#, Github"}
-                description={"Desktop Overlay Companion with Pomodoro and wellness checks through periodic self-care reminders."}
+                description={"Desktop Overlay Companion with Pomodoro and wellness checks through periodic self-care reminders. Developed fully in Unity alongside two teammates during McGill CodeJam 2024."}
                 images={[duck, duckView]}
                 link={"https://devpost.com/software/producktive"}
                 linkName={"Devpost Page"}
